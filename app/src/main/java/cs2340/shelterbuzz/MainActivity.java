c587f144a41c657c2d19d13e469c3034e8a16fb3
ykim0113 <ykim0113@gmail.com>
2018-02-12 21:25:24 -0500

    Logout Button

--- app/src/main/java/cs2340/shelterbuzz/MainActivity.java
+++ app/src/main/java/cs2340/shelterbuzz/MainActivity.java
@@ -1,7 +1,10 @@
 package cs2340.shelterbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent WelcomeActivityIntent = new Intent(getApplicationContext(),
+                        WelcomeActivity.class);
+                startActivity(WelcomeActivityIntent);
+
+            }}
+
+        );
     }
 }
--- app/src/main/java/cs2340/shelterbuzz/WelcomeActivity.java
+++ app/src/main/java/cs2340/shelterbuzz/WelcomeActivity.java
@@ -12,6 +12,7 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
         Button loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
