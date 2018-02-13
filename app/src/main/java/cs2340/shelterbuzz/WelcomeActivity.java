abbd99c64268aa67e35ac47b04004b69f0217b57
ykim0113 <ykim0113@gmail.com>
2018-02-12 20:20:40 -0500

    Welcome Acitivty

---
+++ app/src/main/java/cs2340/shelterbuzz/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package cs2340.shelterbuzz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent LoginActivityIntent = new Intent(getApplicationContext(),
+                        LoginActivity.class);
+                startActivity(LoginActivityIntent);
+
+            }}
+
+        );
+
+
+
+
+
+    }
+
+
+}
