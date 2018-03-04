f4aaca45f92f743fd3ef6a5d6063ee19c5839d49
ykim0113 <ykim0113@gmail.com>
2018-03-03 19:56:05 -0500

    enums with string list

--- app/src/main/java/cs2340/shelterbuzz/model/Gender.java
+++ app/src/main/java/cs2340/shelterbuzz/model/Gender.java
@@ -1,5 +1,8 @@
 package cs2340.shelterbuzz.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by tonyw on 2/24/2018.
  */
@@ -10,13 +13,14 @@ public enum Gender {
     GENDERFLUID("Gender Fluid"),
     GENDERNEUTRAL("Agender");
 
-    private final String[] gender;
+    private final List<String> gender;
 
-    Age(String ... keyword) {
-        this.gender = keyword;
+    Age(String ... keywords) {
+        this.gender = Arrays.asList(keywords);
     }
 
-    public String[] getGender() {
+    public List<String> getGender() {
         return gender;
     }
+
 }
